47109abc816345ab6348814a2c6a31dea47e0133
KyryloKireiev <kirillkireev888@gmail.com>
2023-06-14 14:04:58 +0300

    Initial commit, create plugin infrastructure

---
+++ edx_plugin/apps.py
@@ -0,0 +1,13 @@
+"""
+edx_plugin Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class EdxPluginConfig(AppConfig):
+    """
+    Configuration for the edx_plugin Django application.
+    """
+
+    name = 'edx_plugin'
