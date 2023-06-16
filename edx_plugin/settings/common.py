eb3be49a66b6d2b78a544ba2d2baeb1e586829f7
KyryloKireiev <kirillkireev888@gmail.com>
2023-06-16 11:16:43 +0300

    plugin works, for now plugin's templates in lms/templates

--- edx_plugin/settings/common.py
+++ edx_plugin/settings/common.py
@@ -1,3 +1,4 @@
 def plugin_settings(settings):
     # Update the provided settings module with any app-specific settings.
     settings.FEATURES['ENABLE_MY_APP'] = True
+    settings.PLUGIN_PAGE_URL = "/plugin_page/"
--- edx_plugin/views.py
+++ edx_plugin/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
+from common.djangoapps.edxmako.shortcuts import render_to_response
+from openedx.core.djangoapps.programs.models import ProgramsApiConfig
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET
 
 
 # test response
 def get_test_plugin(request):
     return HttpResponse("This is plugin page")
+
+
+@login_required
+@require_GET
+def get_plugin_page(request):
+    programs_config = ProgramsApiConfig.current()
+    context = {
+        'disable_courseware_js': True,
+        'nav_hidden': True,
+        'show_dashboard_tabs': True,
+        'show_program_listing': programs_config.enabled,
+        'uses_bootstrap': True,
+    }
+    return render_to_response('plugin_page.html', context)
